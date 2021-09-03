9eb571ec3b8eb67b6606ed8b4851866f2b0deff1
Lakyn <lakynmphotography@hotmail.com>
2021-09-03 15:30:38 -0400

    finsihed artist card template and added mock data

--- server.js
+++ server.js
@@ -2,24 +2,28 @@
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+ const path = require("path");
 
-
-var app = express();
+// instantiate the server
+const app = express();
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render('home');
 });
 
-const { artists } = require('./db/artist-seeds.json');
+// const { artists } = require('./db/artist-seeds.json');
 
 // 
 const PORT = process.env.PORT || 3001;
 
-// instantiate the server
-const app = express();
+
 
 
 // function filterByQuery(query, artistsArray) {
@@ -45,12 +49,12 @@ const app = express();
 //     res.json(results);
 // });
 
-
-
-
+const routes = require("./controllers");
+app.use("/", routes);
 
 
 // tell server to listen for requests
-app.listen(3001, () => {
-    console.log(`API server now on port ${PORT}!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port http://localhost:${PORT}!`);
   });
+
