196f77dd04d3134d24dd8589c7c2b212c88a5101
anthonycc <anthonyccardozo@gmail.com>
2021-09-07 18:10:54 -0400

    Add Heroku

--- controllers/index.js
+++ controllers/index.js
@@ -9,9 +9,9 @@ router.use("/", htmlRoutes)
 
 
 
-const router = require('express').Router();
+// const router = require('express').Router();
 
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
