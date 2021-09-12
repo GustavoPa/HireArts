fcb4c09004f4f0309cba576f5362f9bd551803e9
Lakyn <lakynmphotography@hotmail.com>
2021-09-12 13:55:38 -0400

    changes

--- controllers/index.js
+++ controllers/index.js
@@ -8,20 +8,11 @@ router.use("/", htmlRoutes)
 
 
 
-
-const router = require('express').Router();
-
-const apiRoutes = require('./api');
-
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
 });
-
-
-
-
 
 
 module.exports = router;
